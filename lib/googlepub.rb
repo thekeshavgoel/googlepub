52666a9a2569abe799fc225eb4392a4975c70095
thekeshavgoel <keshavgoel@mazdigital.com>
2015-10-26 01:53:41 +0530

    Dependencies correction, version bump

--- lib/googlepub.rb
+++ lib/googlepub.rb
@@ -5,7 +5,7 @@ module Googlepub
 
   ROOT = File.expand_path(File.dirname(__FILE__) + '/..')
 
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 
   def self.call_metadata(options = {})
     @pack = Googlepub::Metadata.new(options["language"])
